2d7a47255e3a5a84971ed08b8fd711ca2871b2fe
Nikita Pivovarov <nikita.pivovarov2003@gmail.com>
2023-10-26 14:37:49 +0300

    Add: initial events

--- Assets/Scripts/Controllers/Utility/GlobalEventController.cs
+++ Assets/Scripts/Controllers/Utility/GlobalEventController.cs
@@ -1,9 +1,41 @@
+using System;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 
 namespace Controllers.Utility
 {
     public class GlobalEventController: MonoBehaviour
     {
+        public static readonly UnityEvent OnEnemyKilled = new UnityEvent();
+        public static readonly UnityEvent OnBattleStarted = new UnityEvent();
+        public static readonly UnityEvent OnBattleEnded = new UnityEvent();
+        public static readonly UnityEvent OnPlayerDeath = new UnityEvent();
+        public static readonly UnityEvent<Scene> OnNewLocationEntered = new UnityEvent<Scene>();
+
+        public static void SendOnEnemyKilled()
+        {
+            OnEnemyKilled.Invoke();
+        }
         
+        public static void SendOnBattleStarted()
+        {
+            OnBattleStarted.Invoke();
+        }
+        
+        public static void SendOnBattleEnded()
+        {
+            OnBattleEnded.Invoke();
+        }
+        
+        public static void SendOnPlayerDeath()
+        {
+            OnPlayerDeath.Invoke();
+        }
+        
+        public static void SendOnNewLocationEntered(Scene scene)
+        {
+            OnNewLocationEntered.Invoke(scene);
+        }
     }
 }
