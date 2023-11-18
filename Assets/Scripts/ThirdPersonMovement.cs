4fb0aab9c73151b734a2186cddf57261473f866c
Nikita Pivovarov <nikita.pivovarov2003@gmail.com>
2023-11-18 21:06:18 +0300

    update: player, add player to main scene (not battle)

--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -9,14 +9,36 @@ namespace DefaultNamespace
         [SerializeField] private Transform cam;
         [SerializeField] private Animator animator;
         [SerializeField] private float speed = 6f;
+        [SerializeField] private float runSpeed = 12f;
         [SerializeField] private float turnSmoothTime = 0.1f;
         [SerializeField] private int timeUntilIdle = 10;
         private float turnSmoothVelocity;
         private float idleTime = 0;
         private bool isIdle = true;
+        
+        public float gravity = -9.81f;
+        public Transform groundCheck;
+        public float groundDistance = 0.4f;
+        public LayerMask groundMask;
+        Vector3 velocity;
+        bool isGrounded;
+        
+        public AudioClip footStepSound;
+        public float footStepDelay;
+ 
+        private float nextFootstep = 0;
+
+        private bool isRunning = false;
 
         private void Update()
         {
+            isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+            
+            if (isGrounded && velocity.y <0)
+            {
+                velocity.y = -2f;
+            }
+            
             float horizontal = Input.GetAxisRaw("Horizontal");
             float vertical = Input.GetAxisRaw("Vertical");
             Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
@@ -24,7 +46,16 @@ namespace DefaultNamespace
             if (horizontal != 0 || vertical != 0)
             {
                 animator.ResetTrigger("Pause");
-                animator.SetTrigger("Go");
+                if (isRunning)
+                {
+                    animator.SetTrigger("Run");
+                    animator.ResetTrigger("Go");
+                }
+                else
+                {
+                    animator.SetTrigger("Go");
+                    animator.ResetTrigger("Run");
+                }
                 animator.ResetTrigger("Idle");
                 isIdle = false;
             }
@@ -39,6 +70,7 @@ namespace DefaultNamespace
                         animator.SetTrigger("Pause");
                         animator.ResetTrigger("Go");
                         animator.ResetTrigger("Idle");
+                        animator.ResetTrigger("Run");
                     }
                 }
                 else
@@ -52,6 +84,7 @@ namespace DefaultNamespace
                 animator.ResetTrigger("Pause");
                 animator.ResetTrigger("Go");
                 animator.SetTrigger("Idle");
+                animator.ResetTrigger("Run");
                 idleTime = 0;
             }
 
@@ -62,7 +95,43 @@ namespace DefaultNamespace
                 transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
                 Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-                controller.Move(moveDirection.normalized * speed * Time.deltaTime);
+                if (isRunning)
+                {
+                    controller.Move(moveDirection.normalized * runSpeed * Time.deltaTime);
+                }
+                else
+                {
+                    controller.Move(moveDirection.normalized * speed * Time.deltaTime);
+                }
+            }
+            
+            velocity.y += gravity * Time.deltaTime;
+            controller.Move(velocity * Time.deltaTime);
+
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                isRunning = true;
+            }
+            else
+            {
+                isRunning = false;
+            }
+            
+            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.W) && isGrounded)
+            {
+                nextFootstep -= Time.deltaTime;
+                if (nextFootstep <= 0) 
+                {
+                    GetComponent<AudioSource>().PlayOneShot(footStepSound, 0.7f);
+                    if (!isRunning)
+                    {
+                        nextFootstep += footStepDelay;
+                    }
+                    else
+                    {
+                        nextFootstep += footStepDelay / 1.5f;
+                    }
+                }
             }
         }
     }
